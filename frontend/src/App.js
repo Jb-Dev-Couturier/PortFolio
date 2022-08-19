f311f1b5055583d1b27274099b0104883c4bdc9a
JB-Dev <jibaoke@gmail.com>
2022-08-19 20:21:19 +0200

    file et folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,49 @@
-import React from 'react'
-import './animationBG.css';
+import React from 'react';
 
+import { About, Footer, Header, Skills, Testimonials, Work } from './container';
+import { Navbar } from './components';
+
+import './animationBG.css';
+import './App.scss'
 
 const App = () => {
   return (
     <>
-    <div class="background">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
+      <div class="background">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
 
-    <h1>App</h1>
-    
+      <div className="app">
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonials />
+        <Footer />
+      </div>
     </>
   );
-}
+};
 
-export default App
+export default App;
