0f9cc9080c0d446ee77cc8bf7114c3e9e4d61844
JB-Dev <jibaoke@gmail.com>
2022-08-19 19:39:03 +0200

    init app

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './animationBG.css';
+
+
+const App = () => {
+  return (
+    <>
+    <div class="background">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+
+    <h1>App</h1>
+    
+    </>
+  );
+}
+
+export default App
